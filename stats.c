89e9937da8e0f69147facd38b8c2a0fc36308958
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2021-10-22 23:46:33 +0530

    Update stats.c
    TC01 - min max avg

--- stats.c
+++ stats.c
@@ -1,10 +1,46 @@
 #include "stats.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
+    struct Stats
+    {
+      float average;
+      float min;
+      float max;  
+    };
+    
     struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+
+    struct Stats compute_statistics(const float* numberset, int setlength)
+    {
+        float sum = 0;
+        int i;
+        
+        /* average computation */
+        for (i = 0; i < setlength; i++) 
+        {
+            sum += numberset[i];
+        }
+        
+        float mn, mx;
+        mn = mx = numberset[0];
+        
+        /* min max computation */
+        for(i=1; i<setlength; i++)
+        {
+            if(numberset[i]>mx)
+            {
+                mx = numberset[i];
+            }
+
+            if(numberset[i]<mn)
+            {
+                mn = numberset[i];
+            }
+        }
+        
+        s.average = sum/setlength ;
+        s.min = mn;
+        s.max = mx;
+    }
 }
 
 int emailAlertCallCount = 0;
