c16941f74e14ff1025b0ce3fdfef5fefcd4b6750
Mukesh <90908610+MukeshKumar77@users.noreply.github.com>
2021-10-23 03:48:46 +0530

    Create alerter.c

---
+++ alerter.c
@@ -0,0 +1,32 @@
+#include "stats.h"
+#include "alert.h"
+
+int emailAlertCallCount;
+int ledAlertCallCount;
+
+//counter to check if both emailAlerter, ledAlerter were called
+int emailAlertFunctionCall = 0;
+int ledAlerterCountFunctionCall = 0;
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+  if(computedStats.max > maxThreshold)
+  {
+      //call email and led Alerter
+      (*alerters[0])();
+      (*alerters[1])();
+       
+      emailAlertFunctionCall++;
+      ledAlerterCountFunctionCall++;
+  }
+}
+
+void emailAlerter()
+{
+    emailAlertCallCount++;
+}
+
+void ledAlerter()
+{
+    ledAlertCallCount++;
+}
